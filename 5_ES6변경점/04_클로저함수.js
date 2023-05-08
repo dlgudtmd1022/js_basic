6f4a55c965edfda10e4d10bf380ac865db429a92
dlgudtmd <dlgudtmd1022@gmail.com>
2023-05-08 18:09:22 +0900

    day 04

---
+++ 5_ES6변경점/04_클로저함수.js
@@ -0,0 +1,22 @@
+function calculator(n1, n2){
+
+    function add(){
+        return n1 + n2;
+    }
+    return add;
+}
+
+const closure =  calculator(5,6);
+// console.log(typeof closure);
+// console.log(closure);
+
+// 즉시 실행 함수
+// 함수를 선언하고, 선언부뒤에 ();를 추가로 붙이면 그 함수는
+// 선언과 동시에 실행까지 됩니다.
+// 일회성으로 호출할 함수를 만들때 많이 사용합니다.
+const start = (() => {
+    console.log("즉시 실행 구문이 붙었군요!")
+})();
+
+// 리턴자료가 없었기 때문에, start 변수에 저장받은 자료가 없음
+// start();
