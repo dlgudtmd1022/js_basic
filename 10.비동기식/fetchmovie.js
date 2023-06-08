2522786d6b9d58ef49a25fc0048c1eb8640960e1
이형승 <dlgudtmd1022@gmail.com>
2023-06-08 17:12:31 +0900

    2023-06-08

---
+++ 10.비동기식/fetchmovie.js
@@ -0,0 +1,5 @@
+async function boxOffice(){
+    const response = await fetch(`http://kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=f5eef3421c602c6cb7ea224104795888&targetDt=20230505`) //GET방식
+    const jsonData = await response.json();
+    console.log(jsonData);
+}
